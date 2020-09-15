3fb1a4515a01661b081dffec1513e5f1c13ef955
chulei <lei.chu@birdsat.cn>
2020-09-15 10:49:05 +0800

    组合模式

---
+++ JavaReview/src/main/java/designpatterns/compositepattern/Employee.java
@@ -0,0 +1,43 @@
+package designpatterns.compositepattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by CHULEI on 2020/9/15.
+ */
+
+class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", dept='" + dept + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
